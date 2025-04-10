76667ec91fb060051b37b602e6b144d92dffa03b
VT7org <glitchnaxusofficial@gmail.com>
2025-04-11 03:14:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,31 +1,30 @@
 import os
 import logging
 from telethon import TelegramClient
-from os import getenv
-from strings.helpers import DEV
 from dotenv import load_dotenv
+from strings.helpers import DEV
 
-logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s', level=logging.WARNING)
-
-API_ID = 18136872
-API_HASH = "312d861b78efcd1b02183b2ab52a83a4"
-
-# Load environment variables
+# Load environment variables from .env file
 load_dotenv()
 
-# Load values from environment variables
-OWNER_ID = int(getenv("OWNER_ID", default="6257927828"))
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", default="6257927828").split()))
-SUDO_USERS.append(OWNER_ID)
-SUDO_USERS.extend(DEV)  # Add DEV users if any
+# Logging configuration
+logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s', level=logging.WARNING)
 
-SPOILER_MODE = os.environ.get("SPOILER_MODE", "True").lower() == "true"
-MONGO_URI = "mongodb+srv://kunaalkumar0091:6qhyyQIyS2idoGFQ@cluster0.z2jge.mongodb.net/?retryWrites=true&w=majority"
-DB_NAME = "kunaalkumar0091"
-LOGGER = False
-BOT_NAME = "Guardify"
-SUPPORT_ID = -1002440907656
-BOT_TOKEN = getenv("BOT_TOKEN", default=None)
+# Load values from environment variables
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+OWNER_ID = int(os.getenv("OWNER_ID"))
+SUDO_USERS = list(map(int, os.getenv("SUDO_USERS", str(OWNER_ID)).split()))
+SUDO_USERS.append(OWNER_ID)
+SUDO_USERS.extend(DEV)
+
+SPOILER_MODE = os.getenv("SPOILER_MODE", "True").lower() == "true"
+MONGO_URI = os.getenv("MONGO_URI")
+DB_NAME = os.getenv("DB_NAME", "default_db_name")
+LOGGER = os.getenv("LOGGER", "False").lower() == "true"
+BOT_NAME = os.getenv("BOT_NAME", "Guardify")
+SUPPORT_ID = int(os.getenv("SUPPORT_ID", "-1002440907656"))
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 # Initialize the bot client
 BOT = TelegramClient('ꜱ ᴛ ᴏ ʀ ᴍ', API_ID, API_HASH).start(bot_token=BOT_TOKEN)
